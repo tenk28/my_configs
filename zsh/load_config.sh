8b56ae811504252d662d6b0b12e7b175fd5f6fd5
Dmytro Shtrikker <shtrikker28@gmail.com>
2024-02-22 19:40:26 +0200

    update zsh
    Signed-off-by: Dmytro Shtrikker <shtrikker28@gmail.com>

--- zsh/load_config.sh
+++ zsh/load_config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PATH_TO_PLACE="$HOME"
-CONFIG_FILES=(".zshrc")
+CONFIG_FILES=(".zshrc" ".p10k.zsh")
 BINARY_NAME="zsh"
 
 if [ -x "$(command -v $BINARY_NAME)" ]; then
@@ -11,6 +11,9 @@ if [ -x "$(command -v $BINARY_NAME)" ]; then
     echo $file placed to $PATH_TO_PLACE
     cp -r "$(find -name $file)" $PATH_TO_PLACE
   done
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 else
-  echo !!! \"$BINARY_NAME\" not found
+  echo !!! $BINARY_NAME not found
 fi
