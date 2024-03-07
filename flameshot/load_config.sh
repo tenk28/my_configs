0954995e8b65d7f7573a71d9c25d0d34b34de602
Dmytro Shtrikker <shtrikker28@gmail.com>
2024-03-07 12:57:14 +0200

    fix flameshot config

--- flameshot/load_config.sh
+++ flameshot/load_config.sh
@@ -6,6 +6,7 @@ BINARY_NAME="flameshot"
 
 if [ -x "$(command -v $BINARY_NAME)" ]; then
   echo -- Loading $BINARY_NAME configs
+  mkdir -p $PATH_TO_PLACE
   for file in ${CONFIG_FILES[@]}; do
     echo $file placed to $PATH_TO_PLACE
     cp -r "$(find -name $file)" $PATH_TO_PLACE
